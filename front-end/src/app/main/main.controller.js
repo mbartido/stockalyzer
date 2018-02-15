8d9f484998aaa907f9065ed6cc4099dcefc1bc11
Dylan Davidson <dylan@Dylans-MBP-5.hsd1.ca.comcast.net>
2018-02-14 18:29:20 -0800

    linked stocks.js symbols to typeahead search and selection menu

--- front-end/src/app/main/main.controller.js
+++ front-end/src/app/main/main.controller.js
@@ -2,13 +2,14 @@ import {stockList} from '../../assets/json/stocks.js';
 export class MainController {
   constructor ($scope) {
     'ngInject';
+      $scope.realList;
+      this.getrealList($scope);
     $scope.stockList; // = stockList[0].Name;
     this.getStockList($scope);
 
       var _selected;
 
   $scope.selected = undefined;
-  $scope.states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Dakota', 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
 
   $scope.ngModelOptionsSelected = function(value) {
     if (arguments.length) {
@@ -25,6 +26,14 @@ export class MainController {
     },
     getterSetter: true
   };
+  }
+      getrealList($scope) {
+    var realListRet = [];
+    // Put names from stockList in nameList
+    for (var i = 0; i < stockList.length; i++) {
+      realListRet.push(stockList[i]);
+    }
+    $scope.realList = realListRet;
   }
 
   // Get stock names in a scope var
