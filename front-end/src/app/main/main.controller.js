c3cf79a4e81fe73c002fc5e7f79a5363117d48c7
Dylan Davidson <dylan@Dylans-MBP-5.hsd1.ca.comcast.net>
2018-02-15 18:01:33 -0800

    added a search button and empty function for when clicked

--- front-end/src/app/main/main.controller.js
+++ front-end/src/app/main/main.controller.js
@@ -2,32 +2,31 @@ import {stockList} from '../../assets/json/stocks.js';
 export class MainController {
   constructor ($scope) {
     'ngInject';
-      $scope.realList;
-      this.getrealList($scope);
-    $scope.stockList; // = stockList[0].Name;
-    this.getStockList($scope);
-
-      var _selected;
-
-  $scope.selected = undefined;
-
-  $scope.ngModelOptionsSelected = function(value) {
-    if (arguments.length) {
-      _selected = value;
-    } else {
-      return _selected;
-    }
-  };
-
-  $scope.modelOptions = {
-    debounce: {
+    $scope.realList;
+    this.getrealList($scope);
+//    $scope.stockList; // = stockList[0].Name;
+//    this.getStockList($scope);
+      //Start typeahead search bar data
+    var _selected;
+    $scope.selected = undefined;
+    $scope.ngModelOptionsSelected = function(value) {
+      if (arguments.length) {
+        _selected = value;
+      } else {
+        return _selected;
+      }
+    };
+    $scope.modelOptions = {
+      debounce: {
       default: 500,
       blur: 250
-    },
+      },
     getterSetter: true
-  };
+    };
+      //End typeahead searchbar data
   }
-      getrealList($scope) {
+
+  getrealList($scope) {
     var realListRet = [];
     // Put names from stockList in nameList
     for (var i = 0; i < stockList.length; i++) {
@@ -36,14 +35,18 @@ export class MainController {
     $scope.realList = realListRet;
   }
 
-  // Get stock names in a scope var
-  getStockList($scope) {
-    var stockListRet = [];
-    // Put names from stockList in nameList
-    for (var i = 0; i < stockList.length; i++) {
-      stockListRet.push(stockList[i].Name);
-    }
-    $scope.stockList = stockListRet;
-    console.log($scope.nameList);
+  searchClicked(){
+      console.log("clickityclick");
   }
+
+//  // Get stock names in a scope var
+//  getStockList($scope) {
+//    var stockListRet = [];
+//    // Put names from stockList in nameList
+//    for (var i = 0; i < stockList.length; i++) {
+//      stockListRet.push(stockList[i].Name);
+//    }
+//    $scope.stockList = stockListRet;
+//    console.log($scope.nameList);
+//  }
 }
