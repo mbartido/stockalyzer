d59eaa899fd9eea3cc6bdc3ee53b71e667075c57
Dylan Davidson <dylan@Dylans-MBP-5.hsd1.ca.comcast.net>
2018-02-15 18:07:33 -0800

    added optional example config data for chart.js in module

--- front-end/src/app/index.module.js
+++ front-end/src/app/index.module.js
@@ -14,29 +14,40 @@ angular.module('stockalyzerFront', ['ui.router', 'ui.bootstrap', 'toastr', 'char
   .constant('moment', moment)
   .config(config)
   .config(routerConfig)
+    //optional example configuration for chart.js, can implement/modify if wanted
+//  .config(['ChartJsProvider', function (ChartJsProvider) {
+//    // Configure all charts
+//    ChartJsProvider.setOptions({
+//      chartColors: ['#FF5252', '#FF8A80'],
+//      responsive: false
+//    });
+//    // Configure all line charts
+//    ChartJsProvider.setOptions('line', {
+//      showLines: true
+//    });
+//  }])
   .run(runBlock)
   .service('githubContributor', GithubContributorService)
   .service('webDevTec', WebDevTecService)
   .controller('MainController', MainController)
-.controller("LineCtrl", ['$scope', '$timeout', function ($scope, $timeout) {
-
-  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-  $scope.series = ['Series A', 'Series B'];
-  $scope.data = [
-    [65, 59, 80, 81, 56, 55, 40],
-    [28, 48, 40, 19, 86, 27, 90]
-  ];
-  $scope.onClick = function (points, evt) {
-    console.log(points, evt);
-  };
-
-  // Simulate async data update
-  $timeout(function () {
+    //Controller for chart.js
+  .controller("LineCtrl", ['$scope', '$timeout', function ($scope, $timeout) {
+    $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+    $scope.series = ['Series A', 'Series B'];
     $scope.data = [
-      [28, 48, 40, 19, 86, 27, 90],
-      [65, 59, 80, 81, 56, 55, 40]
+      [65, 59, 80, 81, 56, 55, 40],
+      [28, 48, 40, 19, 86, 27, 90]
     ];
-  }, 3000);
-}])
+    $scope.onClick = function (points, evt) {
+      console.log(points, evt);
+    };
+    // Simulate async data update
+    $timeout(function () {
+      $scope.data = [
+        [28, 48, 40, 19, 86, 27, 90],
+        [65, 59, 80, 81, 56, 55, 40]
+      ];
+    }, 3000);
+  }])
   .directive('acmeNavbar', NavbarDirective)
   .directive('acmeMalarkey', MalarkeyDirective);
