b32f1246c61b99fdf7270fc7e5d062cbe159a6b8
Michael Bartido <jamberjong@gmail.com>
2018-03-03 20:54:58 -0800

    Styled graph

--- front-end/src/app/main/line.controller.js
+++ front-end/src/app/main/line.controller.js
@@ -2,16 +2,12 @@ export class LineController {
    constructor($scope, $timeout, $interval, priceList){
       'ngInject';
       $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-      // $scope.labels = priceList.date_list;
-      $scope.series = ['Series A', 'Series B'];
-      // $scope.series = ['Stock A'];
+      $scope.series = ['Series A'];
       $scope.data = [
-         [65, 59, 80, 81, 56, 55, 40],
-         [28, 48, 40, 19, 86, 27, 90] 
+         [0,1,2,3,4,5,6],
+         [0,1,2,3,4,5,6] 
       ];
       
-      // $scope.data = [];
-      // $scope.data.push(priceList.price_list);
       $scope.onClick = function (points, evt) {
          console.log(points, evt);
       };
@@ -21,5 +17,30 @@ export class LineController {
          $scope.series = ['Series A'];
          $scope.data = priceList.price_list;
       }.bind(this), 1000)
+
+      $scope.lineOptions = {
+         elements: {
+             line: {
+                 tension: 0, // disables bezier curves
+                 fill: origin,
+                 borderColor: '#98AFC7'
+             }
+         },
+         scales: {
+            xAxes:[{
+               scaleLabel: {
+                  display: true,
+                  labelString: 'Dates'
+               }
+            }],
+            yAxes:[{
+               scaleLabel: {
+                  display: true,
+                  labelString: 'Price (USD)'
+               }
+            }]
+         }
+      }
+
    }
 }
